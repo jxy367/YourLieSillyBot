faf7051f6402ef9eea43e175a9d1e43f2b8a0e52
jxy367 <jxy367@case.edu>
2018-04-26 23:49:52 -0400

    YourLieBot working

--- YourLieSillyBot.py
+++ YourLieSillyBot.py
@@ -1,8 +1,8 @@
 import discord
 from discord.ext import commands
 
-TOKEN = ''
-bot = commands.Bot(command_prefix='$')
+TOKEN = 'NDM5MjQ5MzkwODE4MDMzNjc0.DcQaag.yYxS2Efa_eMqu1vmQCUNrLy8AmI'
+client = commands.Bot(command_prefix='$')
 
 # normal functions #
 
@@ -19,6 +19,7 @@ def get_next_word(current_string):
             new_string = new_string.replace("!", "")
             new_string = new_string.replace(".", "")
             new_string = new_string.replace('"', "")
+            end_index = current_string.find(new_string) + len(new_string)
             if "'" in new_string:
                 split_word = new_string.split("'")
                 if not is_english_word(split_word[0]):  # Contraction
@@ -27,9 +28,12 @@ def get_next_word(current_string):
                 if not is_english_word(new_string):  # Actual English word
                     new_string = ""
         else:
+            print("Woo! New line!")
             return "", ""  # Ran out of possible words
 
-    return new_string, "".join(split_string)
+    print("new word: " + new_string)
+    print("new partial line: " + current_string[end_index:])
+    return new_string, current_string[end_index:]
 
 
 def is_english_word(word):
@@ -37,35 +41,61 @@ def is_english_word(word):
 
 
 def current_percentage():
-    return str(100 * current_line_num / total_lines) + "%"
+    return str("%.2f" % (100 * current_line_num / total_lines)) + "%"
 
 # Bot Commands
 
 
-@bot.command
+@client.command()
 async def progress(ctx):
     await ctx.send(current_percentage() + " Complete!")
 
 
-@bot.command
+@client.command()
 async def line(ctx):
-    start_css = "```css\n"
-    end_css = "\n```"
-    finished_text = current_line.rstrip(current_partial_line).rstrip(current_word)
-    current_text = " [" + current_word + "] "
-    unfinished_text = current_partial_line
-    msg = start_css + finished_text + current_text + unfinished_text + end_css
-    ctx.send(msg)
+    if current_line_num > 30:
+        start_css = "```css\n"
+        end_css = "\n```"
+        finished_text = current_line.rstrip(current_partial_line).rstrip(current_word)
+        current_text = " [" + current_word + "] "
+        unfinished_text = current_partial_line
+        msg = start_css + finished_text + current_text + unfinished_text + end_css
+        await ctx.send(msg)
+    else:
+        await ctx.send("Complete more to unlock this!")
 
-bot.remove_command('help')
+
+@client.command()
+async def reset(ctx, reset_phrase):
+    global current_line_num
+    global current_word
+    global current_partial_line
+    global current_line
+    global your_lie_subtitles
+    if reset_phrase == "AILYLIA":
+        current_line_num = 0
+        with open("YourLieBotCurrentLocation.txt", 'w') as position_file:
+            position_file.write("0")
+            position_file.close()
+        current_word = ""
+        current_partial_line = ""
+        current_line = ""
+        your_lie_subtitles.close()
+        your_lie_subtitles = open("YourLieSubtitles.txt",'r')
+        current_line = your_lie_subtitles.readline().strip()
+        current_word, current_partial_line = get_next_word(current_line)
+        await ctx.send("You have reset completion progress")
+
+client.remove_command('help')
 
 
-@bot.command()
+@client.command()
 async def help(ctx):
     embed = discord.Embed(title="Completion Bot", description="List of commands:", color=0xeee657)
 
     embed.add_field(name="$progress", value="Gives completion percentage", inline=False)
     embed.add_field(name="$line", value="Gives current line and highlights current word in orange", inline=False)
+    embed.add_field(name="$reset reset_phrase", value="Resets completion progress", inline=False)
     embed.add_field(name="$help", value="Gives this message", inline=False)
 
     await ctx.send(embed=embed)
@@ -74,7 +104,7 @@ async def help(ctx):
 # Bot events
 
 
-@bot.event
+@client.event
 async def on_message(message):
     global current_line_num
     global current_word
@@ -84,21 +114,34 @@ async def on_message(message):
     if message.author.bot:
         return
 
+    if current_word is "":
+        return
+
     if current_word.lower() in message.content.lower():
+        await message.channel.send('```YAY! You used the word "' + current_word.lower() + '"!```')
         current_word = ""
         while current_word is "":
             current_word, current_partial_line = get_next_word(current_partial_line)
-            if current_word is "":
+            if current_word is "":  # Move to next line
                 current_line_num += 1
-                current_line = your_lie_subtitles.readline()
-                current_partial_line = current_line
+                if current_line_num == total_lines:
+                    await message.channel.send('```CONGRATULATIONS! YOU COMPLETED YOUR LIE IN APRIL!```')
+                    return
+                else:
+                    with open("YourLieBotCurrentLocation.txt", 'w') as line_num_file:
+                        line_num_file.write(str(current_line_num))
+                        line_num_file.close()
+                    current_line = your_lie_subtitles.readline()
+                    current_partial_line = current_line
+
+    await client.process_commands(message)
 
 
-@bot.event
+@client.event
 async def on_ready():
     print('Logged in as')
-    print(bot.user.name)
-    print(bot.user.id)
+    print(client.user.name)
+    print(client.user.id)
     print('------')
 
 
@@ -108,27 +151,28 @@ total_lines = 6216
 with open("EnglishWords.txt") as word_file:
     english_words = set(word.strip().lower() for word in word_file)
 
-with open("YourLieBotCurrentLocation", 'r') as current_file:
+with open("YourLieBotCurrentLocation.txt", 'r') as current_file:
     current_line_num = int(current_file.read())
-current_file.close()
+    current_file.close()
 
-with open("YourLieSubtitles.txt", 'r') as your_lie_subtitles:
-    current_line = ""
-    num = 0
-    while num < current_line_num:
-        your_lie_subtitles.readline()
-        num += 1
+your_lie_subtitles = open("YourLieSubtitles.txt", 'r')
+current_line = ""
+num = 0
+while num < current_line_num:
+    your_lie_subtitles.readline()
+    num += 1
 
 current_word = ""
 current_partial_line = ""
 while current_word is "":
-    current_line_num += 1
     current_line = your_lie_subtitles.readline().strip()
     current_word, current_partial_line = get_next_word(current_line)
+    if current_word is "":
+        current_line_num += 1
 
 # Check we have values
 print(current_word)
 print(current_partial_line)
 print(current_line)
 
-bot.run(TOKEN)
+client.run(TOKEN)
